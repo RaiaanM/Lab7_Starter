7fc078e4556339fdd0a5b6d15ad7963446943627
Raiaan Mortuja <107028751+RaiaanM@users.noreply.github.com>
2022-11-14 20:11:42 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,6 @@
 // main.js
 
+
 // CONSTANTS
 const RECIPE_URLS = [
   'https://introweb.tech/assets/json/1_50-thanksgiving-side-dishes.json',
@@ -45,6 +46,15 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if('serviceWorker' in navigator){
+    window.addEventListener('load', loadfunc);
+
+    async function loadfunc(){
+      const reg = await navigator.serviceWorker.register('./sw.js')
+      .then( function f1(){console.log('Successful Registration')}, 
+             function f2(){console.log('Unsuccessful Registration')}); 
+    }
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
   // B3. TODO - Register './sw.js' as a service worker (The MDN article
@@ -68,10 +78,33 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+  let item = JSON.parse(window.localStorage.getItem('recipes'));
+  if(item){
+    return item;
+  }
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  
+
+  item = [];
+  return new  Promise(async function (resolve, reject){ 
+    for(let i = 0; i < RECIPE_URLS.length; i++){
+      try{
+        let j = await fetch(RECIPE_URLS[i]);
+        let k = await j.json();
+        item[i] = k;
+      }catch(error){
+        console.log(error);
+        reject(error);
+      }
+    }
+    console.log(item);
+    saveRecipesToStorage(item);
+    resolve(item);
+  });
+
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
